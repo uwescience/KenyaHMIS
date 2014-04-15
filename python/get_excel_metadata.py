a681c59f6c1f59afbbbee1e06b4775ebaa53bf69
Grégoire Lurton <grlurton@uw.edu>
2014-04-15 12:00:52 -0700

    Handling errors in open_workbook
    Still some issues with absence of props in most of the files

--- python/get_excel_metadata.py
+++ python/get_excel_metadata.py
@@ -2,48 +2,40 @@ import xlrd
 import itertools
 import os
 import sys
+import struct
 
 def combine_paths(directory, files):
-    return [os.path.join(directory, filename) for filename in files]
+    return (os.path.join(directory, filename) for filename in files)
 
 def get_excel_for_district(district_path):
     files = os.walk(district_path)
     files_per_directory = [combine_paths(walk[0],walk[2]) for walk in files]
     all_files = list(itertools.chain(*files_per_directory))
-    return [f for f in all_files if f.endswith('xls') or f.endswith('xlsx')]
+    return (f for f in all_files if f.endswith('xls') or f.endswith('xlsx'))
 
 def get_districts(root_path):
     """Start from the directory containing all the districts. A district is assumed to be any
     directory in root_path."""
-    return [directory for directory in os.listdir(root_path) if os.path.isdir(directory)]
+    return (os.path.join(root_path,directory) for directory in os.listdir(root_path) if os.path.isdir(os.path.join(root_path,directory)))
 
 def get_districts_with_files(root_path):
-    ret = {}
-    for district in get_districts(root_path):
-        ret[district] = get_excel_for_district(district)
-    return ret
+    return ((district, get_excel_for_district(district)) for district in get_districts(root_path))
 
 def get_excel_metadata(filename):
-    print "opening %s" % (filename,)
-    book = xlrd.open_workbook(filename)
-    metadata = (district,)+(book.props["created"] , book.props["creator"] , book.props["modified"] , book.props["last_modified_by"])
+    # print "opening %s" % (filename,)
+    try :
+        book = xlrd.open_workbook(filename , on_demand = True )
+    except :
+        return (filename , "error opening file" )
+    try :
+        metadata = (filename,)+(book.props["created"] , book.props["creator"] , book.props["modified"] , book.props["last_modified_by"])
+    except :
+        metadata = (filename , "file has no props")
     return metadata
 
-district_files = get_districts_with_files(r'C://Users//grlurton//Documents//KenyaHIS//python')
-for district, files in district_files.items():
-    if len(files) == 0:
-        continue   
-    print get_excel_metadata(files[0])
-
     
-district_files = get_districts_with_files(r'J:\LIMITED_USE\PROJECT_FOLDERS\KEN\ART_ABCE\HMIS\Districts')
-for district, files in district_files.items():
-    if len(files) == 0:
-        continue   
-    print get_excel_metadata(files[0])
 
-"""C:\Users\grlurton\Documents\Kenya HIS Data 2008-2011\Districts\Buret"""
-"""J:\LIMITED_USE\PROJECT_FOLDERS\KEN\ART_ABCE\HMIS\Districts"""
-
-
-
+district_files = get_districts_with_files('J:\LIMITED_USE\PROJECT_FOLDERS\KEN\ART_ABCE\HMIS\Districts')
+for district, files in district_files:
+    for filename in files :
+        print get_excel_metadata(filename)
