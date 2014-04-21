8e51ee386ee641d8b2b25b9efd3818e664c5bf7d
Grégoire Lurton <grlurton@uw.edu>
2014-04-21 16:30:40 -0700

    Working with every report type
    Works with every report types. added a loop in final call + list of reports

--- ScreenReports.py
+++ ScreenReports.py
@@ -6,9 +6,13 @@ import datetime
 def Get_Report_Meta_Trace(TraceFile , ReportName):
     MetaTrace = csv.DictReader(open(TraceFile))
     for row in MetaTrace :
-        if row["ReportType"] == ReportName:
+        if row['DateCreation'] != '':
             DateCrea = time.strptime(row['DateCreation'], "%m/%d/%Y %H:%M")
-            Trace = {'Author':row['Author'] , 'DateCreated':datetime.datetime(*DateCrea[:5])}
+            if row["ReportType"] == ReportName:
+                Trace = {'Author':row['Author'] , 'DateCreated':datetime.datetime(*DateCrea[:5]) , 'ReportTest':ReportName}
+        else :
+            if row["ReportType"] == ReportName:
+                Trace = {'Author':row['Author'] , 'DateCreated':'No date' , 'ReportTest':ReportName}
     return Trace
 
 # Classify paths according to name and metadata
@@ -21,29 +25,30 @@ def Get_Valid_Paths(ReportDescriptionFile , MetaTrace):
         DateCrea = datetime.datetime(*DateCrea[:5])
         if ReportDescriptionFile['Author'] in MetaTrace['Author'] :
             if  DateCrea == MetaTrace['DateCreated'] :
-                if ReportDescriptionFile['ReportType'] == '710 - Immunization Summary':
+                if ReportDescriptionFile['ReportType'] == MetaTrace['ReportTest']:
                     PathOk = 'All OK'
                 else :
                     PathOk = 'Name not Ok'
             else :
-                if ReportDescriptionFile['ReportType'] == '710 - Immunization Summary':
+                if ReportDescriptionFile['ReportType'] == MetaTrace['ReportTest']:
                     PathOk = 'Date not Ok'
                 else :
                     PathOk = 'Date and Name not Ok'
         else :
             if  DateCrea == MetaTrace['DateCreated'] :
-                if ReportDescriptionFile['ReportType'] == '710 - Immunization Summary':
+                if ReportDescriptionFile['ReportType'] == MetaTrace['ReportTest']:
                     PathOk = 'Author not Ok'
                 else :
                     PathOk = 'Author and Name not Ok'
             else :
-                if ReportDescriptionFile['ReportType'] == '710 - Immunization Summary':
+                if ReportDescriptionFile['ReportType'] == MetaTrace['ReportTest']:
                     PathOk = 'Author and Date not Ok'
                 else :
                     PathOk = 'Nothing Ok'
     except :
         PathOk  = 'Bad Something'
-    ReportScreen = ((ReportDescriptionFile['Author']  , ReportDescriptionFile['DateCreated'], ReportDescriptionFile['Path'] , PathOk))
+    ReportScreen = ((ReportDescriptionFile['Author']  , ReportDescriptionFile['DateCreated'],
+                     ReportDescriptionFile['Path'] , PathOk , ReportDescriptionFile['ReportType'] , MetaTrace['ReportTest']))
     return ReportScreen
 
 def main(TraceFile , ReportDescriptionFile , ReportType):
@@ -52,9 +57,13 @@ def main(TraceFile , ReportDescriptionFile , ReportType):
     for row in WindowsMeta :
         result = Get_Valid_Paths(row,MetaTrace)
         yield result
-        
+
+ReportTypes = ['105 - Service Delivery Summary' , '705A - Outpatient Summary <5' , '705B - Outpatient Summary >5' , '710 - Immunization Summary' ,
+               '711B - RH, TB, Malaria, HIV & Chanis Summary' , '717 - Service Statistics' , '718 - Inpatient Mortality and Morbidity']
+
 with open('ReportScreen.csv', 'wb') as output :
     writer = csv.writer(output , quoting = csv.QUOTE_MINIMAL)
-    writer.writerow(['Author' , 'DateCreated', 'Path' , 'Status'])
-    for results in main('C:\Users\grlurton\Documents\KenyaHMIS\MetadataTrace.csv','C:\Users\grlurton\Documents\KenyaHMIS\WindowsMetadata.csv','710 - Immunization Summary') :
-        writer.writerow(results)
+    writer.writerow(['Author' , 'DateCreated', 'Path' , 'Status','ReportType','ReportTested'])
+    for report in ReportTypes:
+        for results in main('C:\Users\grlurton\Documents\KenyaHMIS\MetadataTrace.csv','C:\Users\grlurton\Documents\KenyaHMIS\WindowsMetadata.csv',report) :
+            writer.writerow(results)
