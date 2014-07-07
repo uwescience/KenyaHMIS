c3b7640adb7403ed1ba09132dbb8862303da050b
Grégoire Lurton <grlurton@uw.edu>
2014-07-07 11:47:33 -0700

    restructuring extraction scripts

---
+++ extraction/Parse705A.py
@@ -0,0 +1,51 @@
+import csv
+import xlrd
+
+def Get_705A_files(FilesScreen , ReportName):
+    FilesScreened = csv.DictReader(open(FilesScreen))
+    paths = []
+    for row in FilesScreened :
+        if row['Status'] not in ['Nothing Ok' , 'Date and Name not Ok'] and row['ReportType'] == ReportName and row['ReportTested'] != '705B - Outpatient Summary >5':
+            paths.append(row['Path'])
+    return paths
+
+def lookup(section , i , j , merge):
+    merge_cell = [(c[0] , c[2]) for c in merge
+                  if i >= c[0] and i < c[1] and j >= c[2] and j < c[3] ]
+    assert len(merge_cell) <= 1
+    if not merge_cell :
+        return section.cell_value(i , j)
+    return section.cell_value(*merge_cell[0])
+
+def import705A(FilePath, writer):
+    book = xlrd.open_workbook(FilePath , formatting_info  = True )
+    assert book.sheet_names() == ['MOH 705A']
+    sheet = book.sheet_by_name('MOH 705A')
+    START_ROW = 6
+    assert sheet.cell_value(START_ROW,0) == 'DISEASES'
+    for row in xrange(START_ROW+1, sheet.nrows):
+        for col in xrange(2, 2+12):
+            indic = lookup(sheet, row, 1, sheet.merged_cells)
+            month = lookup(sheet, START_ROW, col, sheet.merged_cells)
+            value = lookup(sheet, row, col, sheet.merged_cells)
+            if (sheet.cell_type(row, col) == xlrd.XL_CELL_NUMBER or
+                    sheet.cell_type(row, col) == xlrd.XL_CELL_BLANK):
+                t = [FilePath, indic, month, value ]
+            elif sheet.cell_type(row,col) == xlrd.XL_CELL_TEXT and len(value.strip()) == 0:
+                t = [FilePath, indic, month, value.strip() ]
+            else :
+                continue            
+            writer.writerow(t)
+
+with open('705AData.csv', 'wb') as output:
+    writer = csv.writer(output , delimiter = "\t")
+    cntok = 0
+    cntnonok = 0
+    for filename in sorted(Get_705A_files('C:\Users\grlurton\Documents\KenyaHMIS\ReportScreen.csv' , '705A - Outpatient Summary <5')):
+        try:
+            import705A(filename, writer)
+            cntok += 1
+        except Exception as e:
+            print "skipping {} because {}".format(filename, e)
+            cntnonok += 1
+        print '705a ok' , cntok , '705a non ok' , cntnonok
---
+++ extraction/Parse710.py
@@ -0,0 +1,71 @@
+import csv
+import xlrd
+
+def Get_710_files(FilesScreen , ReportName):
+    FilesScreened = csv.DictReader(open(FilesScreen))
+    paths = []
+    for row in FilesScreened :
+        if row['Status'] not in ['Nothing Ok' , 'Date and Name not Ok'] and row['ReportType'] == ReportName :
+            paths.append(row['Path'])
+    return paths
+
+
+def lookup(section , i , j , merge):
+    merge_cell = [(c[0] , c[2]) for c in merge
+                  if i >= c[0] and i < c[1] and j >= c[2] and j < c[3] ]
+    assert len(merge_cell) <= 1
+    if not merge_cell :
+        return section.cell_value(i , j)
+    return section.cell_value(*merge_cell[0])
+
+def import710(FilePath, writer):
+    book = xlrd.open_workbook(FilePath , formatting_info  = True )
+    assert book.sheet_names() == ['MOH 710 Section A', 'MOH 710 Section B']
+    sectionA = book.sheet_by_name('MOH 710 Section A')
+    A_START_ROW = 7
+    assert sectionA.cell_value(A_START_ROW,0) == 'SECTION A'
+    for row in xrange(A_START_ROW+1, sectionA.nrows):
+        for col in xrange(2, 2+12):
+            indic1 = lookup(sectionA, row, 0, sectionA.merged_cells)
+            indic2 = lookup(sectionA, row, 1, sectionA.merged_cells)
+            month = lookup(sectionA, A_START_ROW, col, sectionA.merged_cells)
+            value = lookup(sectionA, row, col, sectionA.merged_cells)
+            if (sectionA.cell_type(row, col) == xlrd.XL_CELL_NUMBER or
+                    sectionA.cell_type(row, col) == xlrd.XL_CELL_BLANK):
+                t = [FilePath, 'A' , indic1, indic2 ,  month, value ]
+            elif sectionA.cell_type(row,col) == xlrd.XL_CELL_TEXT and len(value.strip()) == 0:
+                t = [FilePath, 'A' , indic1, indic2 ,  month, value.strip() ]
+            else :
+                continue
+            writer.writerow(t)
+ 
+    sectionB = book.sheet_by_name('MOH 710 Section B')
+    assert sectionB.cell_value(1,0) == 'BCG'
+    B_START_ROW = 0
+    for row in xrange(B_START_ROW+1, sectionB.nrows):
+        for col in xrange(2, 2+12):
+            indic1 = lookup(sectionB, row, 0, sectionB.merged_cells)
+            indic2 = lookup(sectionB, row, 1, sectionB.merged_cells)
+            month = lookup(sectionB, B_START_ROW, col, sectionB.merged_cells)
+            value = lookup(sectionB, row, col, sectionB.merged_cells)
+            if (sectionB.cell_type(row, col) == xlrd.XL_CELL_NUMBER or
+                    sectionB.cell_type(row, col) == xlrd.XL_CELL_BLANK):
+                t = [FilePath, 'B' , indic1 , indic2, month, value ]
+            elif sectionB.cell_type(row,col) == xlrd.XL_CELL_TEXT and len(value.strip()) == 0:
+                t = [FilePath, 'B' , indic1 , indic2, month, value.strip() ]
+            else :
+                continue
+            writer.writerow(t)
+
+with open('710Data.csv', 'wb') as output:
+    writer = csv.writer(output , delimiter = '\t')
+    cntok = 0
+    cntnonok = 0
+    for filename in sorted(Get_710_files('C:\Users\grlurton\Documents\KenyaHMIS\ReportScreen.csv' , '710 - Immunization Summary')):
+        try:
+            import710(filename, writer)
+            cntok += 1
+        except Exception as e:
+            print "skipping {} because {}".format(filename, e)
+            cntnonok += 1
+        print '710 ok' , cntok , '710 non ok' , cntnonok
