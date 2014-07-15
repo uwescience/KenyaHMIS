6be2b45c10bcfef6f1d49caffdcf15255d3a1cce
grlurton <grlurton@uw.edu>
2014-07-15 16:14:23 -0700

    finalizing matching between distrits and DHIS orgunits
    end product is a shaepfile of these zones.

--- extraction/districts_mapping.r
+++ extraction/districts_mapping.r
@@ -1,4 +1,8 @@
-#### 
+#### This Program takes the districts scraped from DHIS orgunits and matches them as well as possible
+#### with the names in the excel data. 
+#### It then gets the GPS coordinates of these zones from the DHIS2 webapp and produces a shapefile
+#### for the zones in this data.
+
 
 ## Author : Grégoire Lurton
 ## Date   : July 2014
@@ -6,6 +10,12 @@
 library(sqlshare)
 library(stringr)
 library(plyr)
+library(RCurl)
+library(XML)
+library(sp)
+library(maptools)
+library(shapefiles)
+
 
 setwd("J:/Project/abce/ken/HMIS")
 
@@ -57,6 +67,46 @@ colnames(simple_dhis_data) <- c("District" , "url")
 
 districtsTable <- merge(data_districts , simple_dhis_data , by.x = 'districts' , by.y = 'District' , all.x = TRUE , all.y = FALSE)
 
-getGPS <- function(){
-  
+##Return GPS coordinates as a string
+getGPS <- function(url){
+  Page<-getURL(url,userpwd="grlurton:Glurton29",
+               ssl.verifypeer = FALSE , httpauth = 1L)
+  ParsedPage <- xmlParse(Page)
+  root <- xmlRoot(ParsedPage)
+  unitcoordinates <- xmlValue(root[["coordinates"]])
+  unitcoordinates <- substr(unitcoordinates , 4 , nchar(unitcoordinates) - 3)
+  unitcoordinates
 }
+
+## Returns the GPS coordinates in tabular format
+tabGPS <- function(string_coordinates){
+  string_coordinates <- substr(string_coordinates , 2 , nchar(string_coordinates) - 1)
+  tabCoord <- data.frame(unlist(strsplit(string_coordinates , "],[" , fixed = TRUE)))
+  splitcoord <- unlist(strsplit(as.character(tabCoord[,1]) , "," , fixed = TRUE))
+  coord <- data.frame(long = as.numeric(splitcoord[2*(1:(length(splitcoord)/2))-1]) ,
+                      lat = as.numeric(splitcoord[2*(1:(length(splitcoord)/2))]) )
+  if (!is.na(coord$long) & !is.na(coord$lat)){
+    if (coord$long[1] != coord$long[nrow(coord)]) coord$long <- NA
+  }
+  coord
+}
+
+Coordinates <- ddply(districtsTable[!is.na(districtsTable$url) , ] , .(districts) ,
+                     function(x) {print(x$districts)
+                                  tabGPS(getGPS(paste(x$url , "xml" , sep = '.')))} ,
+                     .progress = 'text')
+
+##Taking out non valid coordinates + coordinates from Kentatta National Hospital
+CoordPolygons <- subset(Coordinates , !is.na(long) & !is.na(lat) & districts != "knh")
+
+##Creating data frame to be included in the shapefile
+ShapeData <- data.frame(districts = unique(CoordPolygons$districts) , country = "Kenya")
+
+Shapefile <- convert.to.shapefile(CoordPolygons, ShapeData, "districts", 5)
+write.shapefile(Shapefile, "data/zones", arcgis=T)
+
+Shapefile <- readShapePoly("data/zones.shp")
+Kenya <- readShapePoly('addata/Shapefiles/Districts//kenya_districts98.shp')
+
+plot(Kenya , col = "red")
+plot(Shapefile , add = TRUE , col = "grey")
